532b4081f2854481457a47874a2fc8fcebbe6f2b
cliu822 <ykliu2680@gmail.com>
2025-07-30 19:03:06 -0400

    Add data from OpenDengue Peru

---
+++ R/peru_dengue.R
@@ -0,0 +1,16 @@
+#' Example dataset: peru_dengue
+#'
+#' A brief description of the dataset.
+#'
+#' @format A data frame with 143980 rows and 8 variables:
+#' \describe{
+#'   \item{adm_0_name}{Name of administrative level 0 (Peru)}
+#'   \item{adm_1_name}{Name of administrative level 1 (Department/Region)}
+#'   \item{adm_2_name}{Name of administrative level 2 (Province)}
+#'   \item{calendar_start_date}{Date of start of Epi Week}
+#'   \item{calendar_end_date}{Date of end of Epi Week}
+#'   \item{year}{Year}
+#'   \item{dengue_total}{Total reported dengue cases}
+#'   \item{case_definition_standardised}{Case definition from OpenDengue (ex. suspected, probable, and confirmed)}
+#' }
+#' @source OpenDengue, Clarke J, Lim A, Gupte P, Pigott DM, van Panhuis WG, Brady OJ. OpenDengue: data from the OpenDengue database. Version [1.3]. figshare; 2025. Available from: https://doi.org/10.6084/m9.figshare.24259573.
