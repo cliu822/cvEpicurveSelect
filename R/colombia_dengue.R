cb2d993c2ccbfd04025c1d77c9c52de1dccaadda
cliu822 <ykliu2680@gmail.com>
2025-11-20 12:24:50 -0400

    Swap out peru data for colommbia data

--- R/colombia_dengue.R
+++ R/colombia_dengue.R
@@ -1,12 +1,12 @@
-#' Example dataset: peru_dengue
+#' Example dataset: colombia_dengue
 #'
 #' A brief description of the dataset.
 #'
-#' @format A data frame with 143980 rows and 8 variables:
+#' @format A data frame with 1285200 rows and 8 variables:
 #' \describe{
-#'   \item{adm_0_name}{Name of administrative level 0 (Peru)}
-#'   \item{adm_1_name}{Name of administrative level 1 (Department/Region)}
-#'   \item{adm_2_name}{Name of administrative level 2 (Province)}
+#'   \item{adm_0_name}{Name of administrative level 0}
+#'   \item{adm_1_name}{Name of administrative level 1 (Departaments)}
+#'   \item{adm_2_name}{Name of administrative level 2 (Provinces and subregions)}
 #'   \item{calendar_start_date}{Date of start of Epi Week}
 #'   \item{calendar_end_date}{Date of end of Epi Week}
 #'   \item{year}{Year}
