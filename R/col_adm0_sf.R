a3ff2af0a5140d8edb95b12bba9ed94793c4987a
cliu822 <ykliu2680@gmail.com>
2025-11-20 18:32:44 -0400

    Add in spatial data frames for visualizations

---
+++ R/col_adm0_sf.R
@@ -0,0 +1,11 @@
+#' Example dataset: col_adm0_sf
+#'
+#' A brief description of the dataset.
+#'
+#' @format A spatial data frame of class "sf" with 1 row and 3 variables:
+#' \describe{
+#'   \item{GID_0}{Global ID Country code}
+#'   \item{COUNTRY}{Name of country}
+#'   \item{geometry}{Geometry)}
+#' }
+#' @source
---
+++ R/col_adm2_sf.R
@@ -0,0 +1,12 @@
+#' Example dataset: col_adm2_sf
+#'
+#' A brief description of the dataset.
+#'
+#' @format A spatial data frame of class "sf" with 1119 row and 4 variables:
+#' \describe{
+#'   \item{COUNTRY}{Name of country}
+#'   \item{NAME_1}{Name of admin level 1}
+#'   \item{NAME_2}{Name of admin level 2}
+#'   \item{geometry}{Geometry)}
+#' }
+#' @source
